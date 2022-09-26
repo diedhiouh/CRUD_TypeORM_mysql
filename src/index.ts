d941d6ca6b74c596524ec5fa33fd12cd4dd49e50
Habibou DIEDHIOU <habibodh@gmail.com>
2022-09-26 12:56:22 +0000

    completed basic CRUD mysql

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,12 @@ import bodyParser = require("body-parser")
 let app = express();
 
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 const userRepo = AppDataSource.getRepository(User);
+AppDataSource.initialize();
 
 // let listuser ;
 // AppDataSource.initialize().then(async () => {
@@ -32,32 +36,87 @@ const userRepo = AppDataSource.getRepository(User);
 // }).catch(error => console.log(error))
 
 app.get('/', async (req,res)=>{
-    res.send({
-        'users': await userRepo.find()
+
+    await userRepo.find().then((response) => {
+        res.send({
+            'users': response
+        });
     })
-});
+    .catch((error) => res.json({error: error}));
+})
+  
 
 app.get('/:id', async (req,res) =>{
     // const id = {id: req.params.id}
-    let payload  = 1;
-    const user = await userRepo.findOne({where: { id: payload } })
-    res.json({
-        'message': 'succes',
-        'user': user
+    let payload  = parseInt(req.params.id);
+    
+    await userRepo.findOne({where: { id: payload } })
+    .then((response)=>{
+        res.send({
+            'message': 'user found successfully',
+            'user': response
+        })
     })
+    .catch((error) => res.json({error: error}))
+    
 })
     
-app.post('/', async (req, res)=>{
+app.post("/", async (req, res)=>{
 
-    const user = req.body;
-
-    const usercreation = await userRepo.create(user);
-    const resultat = await userRepo.save(usercreation);
-
-    res.json({
-        'message': 'user created',
-        'user': resultat
+    const luser = req.body;
+    console.log(luser)
+    const usercreation =  userRepo.create(luser);
+    await userRepo.save(usercreation)
+    .then((response) =>{
+        res.send({
+            'message': 'user created',
+            'user': response
+        })
     })
+    .catch((error) => res.json({error: error}))
+
+   
 })
 
-app.listen(process.env.PORT || '3000');
+app.put('/:id', async(req, res) => {
+
+    let payload = req.body;
+
+    const user = await userRepo.findOne({where:{id: parseInt(req.params.id)}});
+
+    const resultat = await userRepo.merge(user, payload);
+
+    userRepo.save(user)
+    .then(() => {
+        res.send({
+            messsage: 'user updated',
+            'user': resultat
+        })
+    })
+    .catch((error) => res.json({error: error}))
+
+
+   
+})
+
+app.delete('/:id', async (req, res) => {
+
+    await userRepo.delete(parseInt(req.params.id))
+    .then((response) => {
+
+        res.send({
+            'message' : 'User Deleted',
+            'user': response
+        })
+    })
+    .catch((error) => res.json({error: error}))
+
+
+    
+})
+
+const PORT = process.env.PORT || '3000'
+
+app.listen(PORT ,() => {
+    console.log('listing port ' + PORT)
+});
